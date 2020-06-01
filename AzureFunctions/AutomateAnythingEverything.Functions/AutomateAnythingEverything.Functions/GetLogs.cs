cd61462e68d48bca765cfd5847982e0e86915f39
lalit@lalitadithya.com <lalit@lalitadithya.com>
2020-06-01 18:51:05 +0530

    Accepting task id via get parameter

--- AzureFunctions/AutomateAnythingEverything.Functions/AutomateAnythingEverything.Functions/GetLogs.cs
+++ AzureFunctions/AutomateAnythingEverything.Functions/AutomateAnythingEverything.Functions/GetLogs.cs
@@ -7,27 +7,31 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using AutomateAnythingEverything.Functions.Services;
 
 namespace AutomateAnythingEverything.Functions
 {
     public class GetLogs
     {
+        private readonly ContainerInstanceService containerInstanceService;
+
+        public GetLogs(ContainerInstanceService containerInstanceService)
+        {
+            this.containerInstanceService = containerInstanceService;
+        }
+
         [FunctionName("GetLogs")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Started processing request!");
 
-            string name = req.Query["name"];
+            string taskId = req.Query["taskId"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
+            string responseMessage = string.IsNullOrEmpty(taskId)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {taskId}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
