2ce5fb0f0ae1e85f8369f2a91002a308b64855c7
lalit@lalitadithya.com <lalit@lalitadithya.com>
2020-06-02 18:49:03 +0530

    Added model for vm details

---
+++ MicrosoftBotFramework/AutomateAnythingEverything.Bot/AutomateAnythingEverything.Bot/Models/VmDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomateAnythingEverything.Bot.Models
+{
+    public class VmDetails
+    {
+        public string VmName { get; set; }
+        public string RgName { get; set; }
+    }
+}
